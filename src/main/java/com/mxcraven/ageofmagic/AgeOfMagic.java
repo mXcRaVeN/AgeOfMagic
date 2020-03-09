01a3955931446626b07136c5d9969dd1ce3f0d96
mXcRaVeN <a0j4price@gmail.com>
2020-03-08 22:24:07 -0400

    more changes

--- src/main/java/com/mxcraven/ageofmagic/AgeOfMagic.java
+++ src/main/java/com/mxcraven/ageofmagic/AgeOfMagic.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("AgeOfMagic")
+@Mod("ageofmagic")
 public class AgeOfMagic
 {
     // Directly reference a log4j logger.
--- src/main/java/com/mxcraven/ageofmagic/ModItems.java
+++ src/main/java/com/mxcraven/ageofmagic/ModItems.java
@@ -1,9 +1,10 @@
 package com.mxcraven.ageofmagic;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
 
 public class ModItems extends Items {
-	   public static final Item AMETHYST_ORE = register(ModBlocks.AMETHYST_ORE, new Item(new Item.Properties()));
+	   public static final Item AMETHYST_ORE = register(ModBlocks.AMETHYST_ORE, new Item(new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
 
 }
